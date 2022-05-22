79404783478d7a77e0fb7fcb31cfc19c00254faa
akki1821 <pppatelakash@gmail.com>
2022-05-22 03:21:46 -0700

    chaged

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [Hori, setHori] = useState(10);
+  const [Veri, setVeri] = useState(10);
+  const [Blur, setBlur] = useState(10);
+  const [color, setColor] = useState("black");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="controls">
+        <label> Horizontal Length</label>
+        <input
+          type="range"
+          min="-100"
+          max="100"
+          value={Hori}
+          onChange={(e) => setHori(e.target.value)}
+        />
+
+        <label>Vertical Length</label>
+        <input
+          type="range"
+          min="-100"
+          max="100"
+          value={Veri}
+          onChange={(e) => setVeri(e.target.value)}
+        />
+
+        <label>Blur</label>
+        <input
+          type="range"
+          min="-100"
+          max="100"
+          value={Blur}
+          onChange={(e) => setBlur(e.target.value)}
+        />
+
+        <label>Color</label>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+      </div>
+
+      <div className="output">
+        <div
+          className="box"
+          style={{ boxShadow: `${Hori}px ${Veri}px ${Blur}px ${color}` }}
         >
-          Learn React
-        </a>
-      </header>
+          <p>
+            boxShadow:{Hori}px
+            {Veri}px {Blur}px {color}{" "}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
